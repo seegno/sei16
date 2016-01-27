7fb6fbc9b2788853ee18c55b89a19cb8992a6e3d
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2016-01-27 22:54:54 +0000

    Add MessageController and `GET /messages` endpoint

---
+++ src/api/controllers/message-controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const messageManager = require('./../managers/message-manager');
+
+/**
+ * Export `MessageController`.
+ */
+
+module.exports = (router) => {
+  // Get messages.
+  router.get('/messages', function *() {
+    this.body = yield messageManager.getMessages();
+  });
+};
