17295a629f424ae2b00ddef0efc7e15eb7d2e5f1
João Granado <jmg@seegno.com>
2016-02-07 18:34:59 +0000

    Add app constants

---
+++ src/client/constants/app-constants.js
@@ -0,0 +1,14 @@
+
+/**
+ * Export `app` constants.
+ */
+
+export const app = {
+  api: {
+    baseUrl: 'http://sei16.seegno.com',
+    endpoints: {
+      messages: 'messages',
+      users: 'users'
+    }
+  }
+};
