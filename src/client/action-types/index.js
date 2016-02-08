2737468418cd29597161d23b02bbe7dc707f84c1
João Granado <jmg@seegno.com>
2016-02-08 00:49:43 +0000

    Add message action types

--- src/client/action-types/index.js
+++ src/client/action-types/index.js
@@ -6,6 +6,7 @@
 import { Enum } from 'enumify';
 import { concat } from 'lodash';
 import ChatActionTypes from 'src/client/action-types/chat-action-types';
+import MessageActionTypes from 'src/client/action-types/message-action-types';
 import UserActionTypes from 'src/client/action-types/user-action-types';
 
 /**
@@ -16,6 +17,7 @@ class ActionTypes extends Enum {}
 
 ActionTypes.initEnum(concat(
   ChatActionTypes,
+  MessageActionTypes,
   UserActionTypes
 ));
 
